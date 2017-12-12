497243e02c1a26f7234a878ceb6923b4a6c6c060
Pedro Vieira <pedro.vieira@gprt.ufpe.br>
2017-12-12 17:27:02 -0300

    Adicionado o espresso e feitos alguns testes.

--- Podcast/app/src/androidTest/java/br/ufpe/cin/if710/podcast/ui/MainActivityTest.java
+++ Podcast/app/src/androidTest/java/br/ufpe/cin/if710/podcast/ui/MainActivityTest.java
@@ -1,6 +1,6 @@
 package br.ufpe.cin.if710.podcast.ui;
 
-
+import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -12,20 +12,25 @@ import android.view.ViewParent;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import br.ufpe.cin.if710.podcast.R;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
@@ -37,17 +42,45 @@ public class MainActivityTest {
 
     @Test
     public void mainActivityTest() {
-        ViewInteraction button = onView(
-                allOf( withId( R.id.item_action ), withText( "Download" ),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName( is( "android.widget.LinearLayout" ) ),
-                                        0 ),
-                                1 ),
-                        isDisplayed() ) );
-        button.perform( click() );
-        button.check(matches( withText( "Downloading..." ) ));
+        /*DataInteraction linearLayout = onData(anything())
+            .inAdapterView(allOf(withId(R.id.items),
+                childAtPosition(
+                    withClassName(is("android.widget.LinearLayout")),
+                    0)))
+            .atPosition(0);
+        linearLayout.perform(click());
 
+        ViewInteraction textView = onView(
+            allOf(withId(R.id.pcTitle), withText("O Homem foi mesmo até a Lua?"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0),
+                    0),
+                isDisplayed()));
+        textView.check(matches(withText("O Homem foi mesmo até a Lua?")));
+
+        pressBack();*/
+
+        ViewInteraction button = onView(
+            allOf(withId(R.id.item_action),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                        0),
+                    1),
+                isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        /*ViewInteraction button2 = onView(
+            allOf(withId(R.id.item_action), withText("DOWNLOAD"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(is("android.widget.LinearLayout")),
+                        0),
+                    0),
+                isDisplayed()));
+        //button2.perform(click());*/
     }
 
     private static Matcher<View> childAtPosition(
@@ -56,15 +89,15 @@ public class MainActivityTest {
         return new TypeSafeMatcher<View>() {
             @Override
             public void describeTo(Description description) {
-                description.appendText( "Child at position " + position + " in parent " );
-                parentMatcher.describeTo( description );
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
             }
 
             @Override
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches( parent )
-                        && view.equals( ((ViewGroup) parent).getChildAt( position ) );
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
---
+++ Podcast/app/src/androidTest/java/br/ufpe/cin/if710/podcast/ui/MainActivityTestRecorded.java
@@ -0,0 +1,156 @@
+package br.ufpe.cin.if710.podcast.ui;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.ufpe.cin.if710.podcast.R;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTestRecorded {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTestRecorded() {
+        DataInteraction linearLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.items),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)))
+                .atPosition(0);
+        linearLayout.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.pcTitle), withText("O Homem foi mesmo até a Lua?"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("O Homem foi mesmo até a Lua?")));
+
+        pressBack();
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.items),
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        linearLayout2.check(matches(isDisplayed()));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.item_action), withText("Download"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button.perform(click());
+
+        DataInteraction linearLayout3 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.items),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)))
+                .atPosition(0);
+        linearLayout3.perform(click());
+
+        pressBack();
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.item_action), withText("Download"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button2.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.editText3),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        editText.perform(pressImeActionButton());
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.item_action), withText("Download"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button3.perform(click());
+
+        DataInteraction linearLayout4 = onData(anything())
+                .inAdapterView(allOf(withId(R.id.items),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)))
+                .atPosition(8);
+        linearLayout4.perform(click());
+
+        pressBack();
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
